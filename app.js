406221c186cc8a6ce955879b1febdd6b0526dcaf
nain93 <73378472+nain93@users.noreply.github.com>
2021-02-12 04:10:51 +0900

    update webpack

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //const express = require('express')
 import express from "express"; //babel (최신의 js버젼 방식으로 변경해줌)and 서버 자동 재시작기능
-import morgan from "morgan";  //morgan = logging에 도움을 주는 middleware
-import helmet from "helmet";  //helmet = 기초보안담당
+import morgan from "morgan"; //morgan = logging에 도움을 주는 middleware
+import helmet from "helmet"; //helmet = 기초보안담당
 import cookieParser from "cookie-parser"; //쿠키를 다룰 수 있음
 import bodyParser from "body-parser"; //form데이터를 서버로 받아와서 활용가능함.
 import { localsMiddleware } from "./middlewareds";
@@ -14,13 +14,18 @@ const app = express();
 /*function handleHome(req, res){
     res.send("Hello from home");
 }*/
-app.set("view engine","pug");
-app.use("/uploads", express.static("uploads")) //주어진 directory에서 file을 전달하는 새로운 middleware fuction
+app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads")); //주어진 directory에서 file을 전달하는 새로운 middleware fuction
+app.use("/static", express.static("static"));
 app.use(cookieParser());
-app.use(bodyParser.json());  //bodyParser
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(helmet());
-app.use(morgan("dev"));      //middleware
+app.use(bodyParser.json()); //bodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+app.use(morgan("dev")); //middleware
 
 app.use(localsMiddleware);
 
@@ -29,4 +34,3 @@ app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
 
 export default app;
-
