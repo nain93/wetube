e3601ca270bb83c2530fd16ee2a1f14ba3ba6169
nain93 <73378472+nain93@users.noreply.github.com>
2021-02-25 02:31:44 +0900

    update css,login

--- app.js
+++ app.js
@@ -2,13 +2,17 @@
 import express from "express"; //babel (최신의 js버젼 방식으로 변경해줌)and 서버 자동 재시작기능
 import morgan from "morgan"; //morgan = logging에 도움을 주는 middleware
 import helmet from "helmet"; //helmet = 기초보안담당
-import cookieParser from "cookie-parser"; //쿠키를 다룰 수 있음
 import bodyParser from "body-parser"; //form데이터를 서버로 받아와서 활용가능함.
+import passport from "passport"; // 로그인 인증
+import session from "express-session"; //
+import cookieParser from "cookie-parser"; //쿠키를 다룰 수 있음
 import { localsMiddleware } from "./middlewareds";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+
+import "./passport";
 
 const app = express();
 /*function handleHome(req, res){
@@ -17,7 +21,6 @@ const app = express();
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads")); //주어진 directory에서 file을 전달하는 새로운 middleware fuction
 app.use("/static", express.static("static"));
-app.use(cookieParser());
 app.use(bodyParser.json()); //bodyParser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
@@ -25,8 +28,17 @@ app.use(
     contentSecurityPolicy: false,
   })
 );
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+  })
+);
 app.use(morgan("dev")); //middleware
-
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cookieParser());
 app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 import "../scss/styles.scss";
+import "../scss/videos.scss";
 
 const something = async () => {
   console.log(something);
